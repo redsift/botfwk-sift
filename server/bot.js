4f53bc04111db99d75f206a6c0b7c208fb1fdc27
Dmitry Motylev <dmitry@redsift.io>
2017-04-19 13:32:50 +0100

    final version

--- server/bot.js
+++ server/bot.js
@@ -4,6 +4,20 @@
 'use strict';
 
 var botfwk = require('./botfwk.js');
+var builder = require('botbuilder');
+
+var appId = process.env._BOTFWK_MICROSOFT_APP_ID || '';
+var appPassword = process.env._BOTFWK_MICROSOFT_APP_PASSWORD || '';
+
+var connector = new builder.ChatConnector({
+  appId: appId,
+  appPassword: appPassword
+});
+
+console.log('bot.js: _BOTFWK_MICROSOFT_APP_ID', appId);
+console.log('bot.js: _BOTFWK_MICROSOFT_APP_PASSWORD', appPassword.replace(/^(.?).*?$/, function (m, p1, o, s) { return p1 + '***' }));
+
+var bot = new builder.UniversalBot(connector);
 
 // Entry point for DAG node
 // got ={
@@ -18,13 +32,28 @@ module.exports = function (got) {
   const inData = got.in;
   var promises = [];
 
-
   for (var d of inData.data) {
     if (d.value) {
       try {
         // parse raw message
         var msg = JSON.parse(d.value);
-        promises.push(botfwk.reply(msg, 'Hello, ' + msg.from.name + '! You wrote ' + msg.text));
+        // extract address in format expected by by Bot Framework SDK
+        var addr = botfwk.address(msg);
+        var text = 'Hello, ' + msg.from.name + '! You wrote ' + msg.text;
+
+        promises.push(
+          new Promise(function (resolve, reject) {
+            var reply = new builder.Message()
+              .address(addr)
+              .text(text);
+            bot.send(reply, function (err) {
+              if (err) {
+                reject(err);
+                return;
+              }
+              resolve();
+            });
+          }));
       } catch (ex) {
         console.error('bot.js: Error parsing value for: ', d.key);
         console.error('bot.js: Exception: ', ex);
